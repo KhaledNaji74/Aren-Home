a77a29ef505c1949d60c8594b044dbf863d628d4
KhaledNaji74 <khalidassaf74@yahoo.com>
2026-09-20 13:20:35 +0400

    Implement memory test endpoint in worker.js
    Added a memory test endpoint to store and retrieve a value from KV.

--- worker.js
+++ worker.js
@@ -1,1 +1,17 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
 
+    // Test Aren's memory
+    if (url.pathname === "/memory-test") {
+      await env.KV.put("test", "Aren memory is working");
+
+      const memory = await env.KV.get("test");
+
+      return new Response(memory);
+    }
+
+    // Serve the existing homepage
+    return env.ASSETS.fetch(request);
+  }
+};
